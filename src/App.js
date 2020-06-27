0b20a9c5d5a96586ca3a9a47dfe4fa6b85b7f673
hariom7492 <hariom7492@gmail.com>
2020-06-27 15:22:55 +0530

    Shop Page & Collection Preview & Collection-Item with basic styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 
+import ShopPage from './pages/shop/shop.component'
+
+
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        < Route exact path='/' component={HomePage}/>
+        < Route  path='/shoppage' component={ShopPage}/>
+      </Switch>
     </div>
   );
 }
